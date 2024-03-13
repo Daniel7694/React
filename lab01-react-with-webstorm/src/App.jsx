3f5a40f10bb1243df1e628ce81a9d212e44ab9d1
Daniel769 <jeam.nieto769@gmail.com>
2024-03-13 14:11:34 -0500

    (frontend):lab01 finished

---
+++ lab01-react-with-webstorm/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App(){
+    return(
+        <>
+            <h1>Bienvenidos a mi pagina</h1>
+            <p>CEET -SENA</p>
+        </>
+    );
+}
+
+export default App;
