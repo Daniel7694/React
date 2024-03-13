13013c74d442f87d7f174e58918203460d8cb099
Daniel769 <jeam.nieto769@gmail.com>
2024-03-13 18:18:55 -0500

    (frontend):finished 03 images and styles

---
+++ 03-agregando-estilos/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App(){
+    return(
+        <>
+            <img className="rounded-full" style={{borderRadius: '50%'}} src="src/assets/images/paisaje.jpeg" alt="paisaje de lago"/>
+        </>
+    )
+}
+export default App
